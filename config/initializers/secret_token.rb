656390762cfcac0f231dcda07bc4e222a309c4bb
Mike <mcarmody2013@gmail.com>
2014-02-03 16:30:46 -0600

    initial commit with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GalleryEighteen::Application.config.secret_key_base = '9550a1c064abe670672959c75f85fd0cdb9a17b0b135efa85e924288f0a7ac2e6f0d7d6dcad7c217b618621d927d17cd03185bea6941b9d724bdb57c3f564d0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GalleryEighteen::Application.config.session_store :cookie_store, key: '_GalleryEighteen_session'
