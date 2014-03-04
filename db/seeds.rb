037fe332080411a43d094494ebb87eb24a04eddb
Mike <mcarmody2013@gmail.com>
2014-03-04 11:32:17 -0600

    tweaked user profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -9,7 +10,7 @@
 Item.destroy_all
 User.destroy_all
 
-1.times do 
+2.times do 
   Item.create(title: "Lilypads", details: "deets", price: 100, pic: 'SimoneAnne-6292.jpg', user_id: 1)
   Item.create(title: "Beach Glass", details: "deets", price: 300, pic: 'SimoneAnne-1862.jpg', user_id: 1)
   Item.create(title: "Surfer Dude", details: "deets", price: 300, pic: 'SimoneAnne-8689.jpg', user_id: 1)
@@ -24,4 +25,4 @@ User.destroy_all
 
 User.create(name: 'Mike Carmody', password: 'password', email: 'mcarmody2013@gmail.com', school: '42.047418, -87.700815', category: 'Photography', pic: 'profile.jpg', uid: 1)
 User.create(name: 'Josh Wood', password: 'password', email: 'sample@example.com', school: '42.095219, -87.717810', category: 'Architecture', pic: 'wood_josh.jpg', uid: 2)
-User.create(name: 'Barack Obama', password: 'password', email: 'sample@example.com', school: '42.095219, -87.717810', category: 'Painting', pic: 'obama.jpg', uid: 3)
+User.create(name: 'Barack Obama', password: 'password', email: 'sample2@example.com', school: '42.095219, -87.717810', category: 'Painting', pic: 'obama.jpg', uid: 3)
