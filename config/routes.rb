85708f3484bbfd70ae976f376e2a447072c74a55
Mike <mcarmody2013@gmail.com>
2014-03-05 13:16:06 -0600

    rerouted root path to items/index

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ GalleryEighteen::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'items#index'
 
   get 'users/omniauth_callbacks', to: 'items#index'
 
